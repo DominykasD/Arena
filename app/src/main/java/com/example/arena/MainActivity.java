a4a61e092568961f54ab7bb5f397762061107268
Dominykas <dominykasdrevinskas@gmail.com>
2022-08-08 11:57:46 +0300

    Login and Register implemented

--- app/src/main/java/com/example/arena/MainActivity.java
+++ app/src/main/java/com/example/arena/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.arena;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView register;
     private Button button;
-    private String url = "localhost/ArenaServer";
+    private EditText email, password, telephone;
+    private String URL = "http://10.0.2.2/ArenaServer/login.php";
 
 
     @Override
@@ -20,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Open Register activity
         register = (TextView) findViewById(R.id.textRegister);
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,14 +45,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        email = (EditText) findViewById(R.id.editEmail);
+        password = (EditText) findViewById(R.id.editPsw);
         button = (Button) findViewById(R.id.buttLogin);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openMainMenuActivity();
+                if (!email.getText().toString().trim().equals("") && !password.getText().toString().trim().equals("")) {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("success")) {
+                                openMainMenuActivity();
+                            } else if (response.equals("failure")) {
+                                Toast.makeText(MainActivity.this, "Neteisingas įvestas paštas arba slaptažodis!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> data = new HashMap<>();
+                            data.put("email", email.getText().toString().trim());
+                            data.put("password", password.getText().toString().trim());
+                            return data;
+                        }
+                    };
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    requestQueue.add(stringRequest);
+                }
             }
         });
     }
+
 
     private void openRegisterActivity() {
         Intent intent = new Intent(this, Register.class);
--- app/src/main/java/com/example/arena/Register.java
+++ app/src/main/java/com/example/arena/Register.java
@@ -5,11 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
     private TextView login;
+    private Button button;
+    private EditText email, password, phoneNumber;
+    private String URL = "http://10.0.2.2/ArenaServer/register.php";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +39,46 @@ public class Register extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 openMainActivity();
+            }
+        });
+
+        email = (EditText) findViewById(R.id.editEmailreg);
+        password = (EditText) findViewById(R.id.editPswreg);
+        phoneNumber = (EditText) findViewById(R.id.editTelreg);
+        button = (Button) findViewById(R.id.buttRegister);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!email.getText().toString().trim().equals("") && !password.getText().toString().trim().equals("") && !phoneNumber.getText().toString().trim().equals("")) {
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("success")) {
+                                Toast.makeText(getApplicationContext(), "Paskyra sukurta!", Toast.LENGTH_SHORT).show();
+//                                openMainActivity();
+                            } else if (response.equals("failure")) {
+                                Toast.makeText(getApplicationContext(), "Įvyko klaida, bandykite dar kartą", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                        }
+                    }) {
+                        @Override
+                        protected Map<String, String> getParams() throws AuthFailureError {
+                            Map<String, String> data = new HashMap<>();
+                            data.put("email", email.getText().toString().trim());
+                            data.put("phoneNumber", phoneNumber.getText().toString().trim());
+                            data.put("password", password.getText().toString().trim());
+                            return data;
+                        }
+                    };
+                    RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+                    requestQueue.add(stringRequest);
+                }
+
             }
         });
     }
