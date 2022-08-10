990850c92943d91160b1a1e9b0ab4e869f2b6f5e
Dominykas <dominykasdrevinskas@gmail.com>
2022-08-10 12:53:00 +0300

    Updated navigation drawer

--- app/src/main/java/com/example/arena/MainMenuActivity.java
+++ app/src/main/java/com/example/arena/MainMenuActivity.java
@@ -2,14 +2,24 @@ package com.example.arena;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.SearchView;
+
+import com.google.android.material.appbar.AppBarLayout;
 
 public class MainMenuActivity extends AppCompatActivity {
 
     private SearchView searchView;
-    private Toolbar toolbar;
+    private Toolbar toolbar, topAppBar;
+    private AppBarConfiguration mAppBarConfiguration;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +27,24 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         /// Searchview text
-        searchView = (SearchView) findViewById(R.id.searchView);
-        searchView.setQueryHint("Ką galėtume pasiūlyti?");
+//        searchView = (SearchView) findViewById(R.id.searchView);
+//        searchView.setQueryHint("Ką galėtume pasiūlyti?");
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationContentDescription("asd");
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        toolbar.setNavigationContentDescription("asd");
+
+//        topAppBar = (Toolbar) findViewById(R.id.topAppBar);
+//        setSupportActionBar(toolbar);
+//        topAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+
+
+
     }
+
 }
